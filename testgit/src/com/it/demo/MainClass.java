3d14202e3de48850f1ae08ce648116e1a7687b9e
LYS <2817788383@qq.com>
2020-05-23 10:09:23 +0800

    提交java项目

---
+++ testgit/src/com/it/demo/MainClass.java
@@ -0,0 +1,10 @@
+package com.it.demo;
+
+/**
+ * Created by LYS on 2020/5/23.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
